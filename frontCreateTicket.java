330d7d9b728cea086826053a71b892afdd8f80fe
EricQiu71 <37564878+EricQiu71@users.noreply.github.com>
2018-03-25 21:38:15 -0400

    Update the Create Ticket file

--- frontCreateTicket.java
+++ frontCreateTicket.java
@@ -6,37 +6,107 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class frontcreateticket extends JFrame {
+public class frontCreateTicket extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frontcreateticket frame = new frontcreateticket();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public frontcreateticket() {
+	public frontCreateTicket() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 764, 528);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("User Key");
+		lblNewLabel.setBounds(88, 63, 46, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(169, 60, 148, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ticket ID");
+		lblNewLabel_1.setBounds(88, 109, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Create Date");
+		lblNewLabel_2.setBounds(88, 154, 81, 14);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Availability Time");
+		lblNewLabel_3.setBounds(88, 195, 81, 14);
+		panel.add(lblNewLabel_3);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(169, 106, 148, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(169, 151, 148, 20);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(169, 192, 148, 20);
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(169, 240, 323, 111);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Description");
+		lblNewLabel_4.setBounds(88, 240, 71, 14);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblCreateTicket = new JLabel("Create Ticket");
+		lblCreateTicket.setFont(new Font("Arial", Font.BOLD, 18));
+		lblCreateTicket.setBounds(275, 11, 131, 26);
+		panel.add(lblCreateTicket);
+		
+		JButton btnCreate = new JButton("Create");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCreate.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnCreate.setBounds(146, 362, 160, 65);
+		panel.add(btnCreate);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnGoBack.setBounds(433, 362, 160, 65);
+		panel.add(btnGoBack);
+		
+		JButton button = new JButton("Log out");
+		button.setBounds(562, 15, 89, 23);
+		panel.add(button);
 	}
-
 }
