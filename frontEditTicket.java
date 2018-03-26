85b0d76fd9b70fc4797a033178cf29cbef3b7136
EricQiu71 <37564878+EricQiu71@users.noreply.github.com>
2018-03-25 21:36:58 -0400

    Delete the main function

--- frontEditTicket.java
+++ frontEditTicket.java
@@ -23,18 +23,7 @@ public class frontEditTicket extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frontEditTicket frame = new frontEditTicket();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the frame.
