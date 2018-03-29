9b87a268487bf4e2ef1c59906854d0de2518c213
EricQiu71 <qiuzijie71@gmail.com>
2018-03-29 18:55:08 -0400

    finish the edit ticket function
    need to change some value

--- frontEditTicket.java
+++ frontEditTicket.java
@@ -7,30 +7,64 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
-public class frontEditTicket extends JFrame {
+import java.awt.Font;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class frontEditTicket2 extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-
+	private JTextField descriptiontextField;
+	private JTextField patientkeytextField;
+	private JTextField ticketidtextField;
+	private JTextField datecreatedtextField;
+	Connection connection = null;
+	//static int ticketid = frontTicketList.ticketid;
+	static int ticketid = 12345;
+	
 	/**
 	 * Launch the application.
 	 */
-
+	public void fillpage() {
+		PreparedStatement stmt = null;
+		 
+		try {
+			
+			String sql;
+			System.out.print(ticketid);
+            sql = "SELECT * FROM ticketTable where ticketid = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, ticketid);
+            ResultSet rs = stmt.executeQuery(); 
+            while(rs.next()) {
+           
+            patientkeytextField.setText(rs.getString("patientkey"));
+            ticketidtextField.setText(rs.getString("ticketid"));
+            descriptiontextField.setText(rs.getString("description"));
+            datecreatedtextField.setText(rs.getString("datecreated"));
+           
+            }
+            }catch(Exception e){
+	           
+            e.printStackTrace();
+        }
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public frontEditTicket() {
+	public frontEditTicket2() {
+		connection = sqlConnection.dbConnector();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 764, 528);
+		setBounds(100, 100, 756, 517);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -40,73 +74,102 @@ public class frontEditTicket extends JFrame {
 		panel.setLayout(null);
 		contentPane.add(panel, BorderLayout.CENTER);
 		
-		JLabel label = new JLabel("User Key");
-		label.setBounds(88, 63, 46, 14);
-		panel.add(label);
+		JLabel patientksylabel = new JLabel("Patient Key:");
+		patientksylabel.setBounds(88, 63, 81, 14);
+		panel.add(patientksylabel);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(169, 60, 148, 20);
-		panel.add(textField);
+		JLabel ticketidlabel = new JLabel("Ticket ID:");
+		ticketidlabel.setBounds(88, 109, 81, 14);
+		panel.add(ticketidlabel);
 		
-		JLabel label_1 = new JLabel("Ticket ID");
-		label_1.setBounds(88, 109, 46, 14);
-		panel.add(label_1);
+		JLabel datecreatedlabel = new JLabel("Date Created:");
+		datecreatedlabel.setBounds(88, 154, 96, 14);
+		panel.add(datecreatedlabel);
 		
-		JLabel label_2 = new JLabel("Create Date");
-		label_2.setBounds(88, 154, 81, 14);
-		panel.add(label_2);
+		JLabel descriptionlabel = new JLabel("Description:");
+		descriptionlabel.setBounds(88, 240, 96, 14);
+		panel.add(descriptionlabel);
 		
-		JLabel label_3 = new JLabel("Availability Time");
-		label_3.setBounds(88, 195, 81, 14);
-		panel.add(label_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(169, 106, 148, 20);
-		panel.add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(169, 151, 148, 20);
-		panel.add(textField_2);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(169, 192, 148, 20);
-		panel.add(textField_3);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(169, 240, 323, 111);
-		panel.add(textField_4);
-		
-		JLabel label_4 = new JLabel("Description");
-		label_4.setBounds(88, 240, 71, 14);
+		JLabel label_4 = new JLabel("Edit Ticket");
+		label_4.setFont(new Font("Arial", Font.BOLD, 18));
+		label_4.setBounds(275, 11, 131, 26);
 		panel.add(label_4);
 		
-		JLabel lblEditTicket = new JLabel("Edit Ticket");
-		lblEditTicket.setFont(new Font("Arial", Font.BOLD, 18));
-		lblEditTicket.setBounds(275, 11, 131, 26);
-		panel.add(lblEditTicket);
+		JButton resubmitbutton = new JButton("Resubmit");
+		resubmitbutton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(descriptiontextField.getText().length()>=10 && descriptiontextField.getText().length()<50) 
+				{try {
+					String sql;
+					 sql = "Update ticketTable set description = '"+descriptiontextField.getText()+"' where ticketid = ? ";
+					 PreparedStatement upd;
+					 upd = connection.prepareStatement(sql);
+					 upd.setInt(1, ticketid);
+					 upd.execute();
+					 JOptionPane.showMessageDialog(null, "Resubmit successfully!");
+				}catch(Exception e1){
+			           
+		            e1.printStackTrace();
+		        }
+				}
+				if(descriptiontextField.getText().length()<10) {
+					JOptionPane.showMessageDialog(null, "Description must be at least 10 characters.");
+				}
+				if(descriptiontextField.getText().length()>50) {
+					JOptionPane.showMessageDialog(null, "Description must be below 50 characters.");
+				}
+			}
+		});
+		resubmitbutton.setFont(new Font("Arial", Font.PLAIN, 18));
+		resubmitbutton.setBounds(105, 364, 160, 65);
+		panel.add(resubmitbutton);
 		
-		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.setFont(new Font("Arial", Font.PLAIN, 18));
-		btnSubmit.setBounds(53, 362, 160, 65);
-		panel.add(btnSubmit);
+		descriptiontextField = new JTextField();
+		descriptiontextField.setColumns(10);
+		descriptiontextField.setBounds(210, 234, 309, 91);
+		panel.add(descriptiontextField);
 		
-		JButton button_1 = new JButton("Go Back");
-		button_1.setFont(new Font("Arial", Font.PLAIN, 18));
-		button_1.setBounds(262, 362, 160, 65);
-		panel.add(button_1);
+		JButton deletebutton = new JButton("Delete");
+		deletebutton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String sql;
+					 sql = "delete from ticketTable where ticketid = ? ";
+					 PreparedStatement upd;
+					 upd = connection.prepareStatement(sql);
+					 upd.setInt(1, ticketid);
+					 upd.execute();
+					 JOptionPane.showMessageDialog(null, "Delete successfully!");
+					 dispose();
+					 frontTicketList doctorinfo = new frontTicketList();
+	            	 doctorinfo.setVisible(true);
+				}catch(Exception e1){
+			           
+		            e1.printStackTrace();
+		        }
+			}
+		});
+		deletebutton.setFont(new Font("Arial", Font.PLAIN, 18));
+		deletebutton.setBounds(471, 364, 160, 65);
+		panel.add(deletebutton);
 		
-		JButton button_2 = new JButton("Log out");
-		button_2.setBounds(562, 15, 89, 23);
-		panel.add(button_2);
+		patientkeytextField = new JTextField();
+		patientkeytextField.setColumns(10);
+		patientkeytextField.setBounds(210, 60, 104, 20);
+		panel.add(patientkeytextField);
 		
-		JButton btnDelete = new JButton("Delete");
-		btnDelete.setFont(new Font("Arial", Font.PLAIN, 18));
-		btnDelete.setBounds(472, 362, 160, 65);
-		panel.add(btnDelete);
+		ticketidtextField = new JTextField();
+		ticketidtextField.setColumns(10);
+		ticketidtextField.setBounds(210, 106, 104, 20);
+		panel.add(ticketidtextField);
+		
+		datecreatedtextField = new JTextField();
+		datecreatedtextField.setBounds(213, 151, 101, 20);
+		panel.add(datecreatedtextField);
+		datecreatedtextField.setColumns(10);
+		
+		fillpage();
+		
+		
 	}
 }
